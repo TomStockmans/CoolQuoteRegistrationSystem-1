346a058b8905a8843502e49d80c885f8cb654009
Tim Schraepen <sch3lp@gmail.com>
2016-10-08 21:38:55 +0200

    split up quote in components

---
+++ src/main/resources/static/src/quotes/line.js
@@ -0,0 +1,7 @@
+import {bindable} from "aurelia-framework";
+
+export class Line {
+
+  @bindable line = null;
+  
+}
--- src/main/resources/static/src/quotes/quote.js
+++ src/main/resources/static/src/quotes/quote.js
@@ -4,7 +4,10 @@ export class Quote {
 
   @bindable quote = null;
 
-  
-
+  lines() {
+    let allLines = this.quote.lines;
+    allLines.push(this.quote.punchLine);
+    return allLines;
+  }
 
 }
