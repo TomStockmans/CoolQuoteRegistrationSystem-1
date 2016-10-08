53d347e0ed106f073795ef0bece24cd7abc4cfc6
Tim Schraepen <sch3lp@gmail.com>
2016-10-08 22:05:10 +0200

    some cleanup

--- src/main/resources/static/src/quotes/participants.js
+++ src/main/resources/static/src/quotes/participants.js
@@ -4,10 +4,4 @@ export class Participants {
   
   @bindable participants = null;
 
-  participants() {
-    if (!this.participants) {
-      return "Nobody";
-    }
-    return this.participants.map(p => p.name).join(", ");
-  }
 }
