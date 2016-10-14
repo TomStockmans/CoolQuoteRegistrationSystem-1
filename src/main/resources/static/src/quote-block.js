b8ba3bbacca833ddbb72ef720a4dbe2332b7f343
bktid <Cegeka01>
2016-10-14 11:05:54 +0200

    bktid:brunol:robs: split quoteblock into lines and inflate a Conversation object refactor

--- src/main/resources/static/src/quote-block.js
+++ src/main/resources/static/src/quote-block.js
@@ -4,7 +4,11 @@ import {Conversation} from './conversation';
 export class QuoteBlock {
     addQoute() {
       let conversation = new Conversation();
-      this.content.split('\n').forEach(conversation.addLine.bind(conversation));
+      
+      this.content.split('\n').forEach(line => conversation.addLine(line));
+      // COMMENT: dit kan je ook doen door conversation.addLine rechtstreeks door te geven, maar dan moet je this binden
+      // this.content.split('\n').forEach(conversation.addLine.bind(conversation));
+      
       new WebAPI().saveQuote(conversation);
     }
   
