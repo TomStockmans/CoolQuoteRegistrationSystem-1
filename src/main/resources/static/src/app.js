bcedf9e32d94f46376328070b23e4a2906636e06
vercruyssetim <tim.vercruysse@cegeka.be>
2016-10-14 12:57:59 +0200

    [Timva]:  - fixen package.json

--- src/main/resources/static/src/app.js
+++ src/main/resources/static/src/app.js
@@ -9,6 +9,7 @@ export class App {
     config.map([
       { route: '', moduleId: 'quotes', title: 'Quotes' },
       { route: 'addQuoteBlock', moduleId: 'quote-block', title: 'Add quote block' },
+      { route: 'addQuoteBlockSpecial', moduleId: 'components/quote-block-special/quote-block-special', title: 'Add quote block special' },
       { route: 'quotes', moduleId: 'quotes', title: 'Quotes' },
       { route: 'searchquotes', moduleId: 'search-quotes', title: 'SearchQuotes' }
     ]);
---
+++ src/main/resources/static/src/components/quote-block-special/quote-block-special.js
@@ -0,0 +1,39 @@
+import {WebAPI} from '../../web-api';
+import {Conversation} from '../../conversation';
+import {Line} from '../../line';
+
+
+function parseRawLine(rawLine) {
+    let parserFunction = getLineParserFunction(rawLine);
+    return parserFunction(rawLine);
+}
+
+function getLineParserFunction(rawLine) {
+    if (rawLine.startsWith('/c:')) {
+        return contextLineParser;
+    }
+    return speechLineParser;
+}
+
+function contextLineParser(rawLine) {
+    return new Line('CONTEXT', rawLine.substring(3));
+}
+
+function speechLineParser(rawLine) {
+    let lineParts = rawLine.split(':',2);
+    let author = lineParts[0];
+    let content = lineParts[1];
+    return new Line('SPEECH', content, author);
+}
+
+export class QuoteBlock {
+    addQoute() {
+        let conversation = new Conversation();
+
+        this.content.split('\n')
+            .map(parseRawLine)
+            .forEach(line => conversation.addLine(line));
+
+        new WebAPI().saveQuote(conversation);
+    }
+}
