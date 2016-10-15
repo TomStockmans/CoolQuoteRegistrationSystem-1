c12cb03e3b640cedc30caf2dd5b5dedfd0024642
Tim Schraepen <sch3lp@gmail.com>
2016-10-15 17:16:56 +0200

    it's so silly that you can't do something like this

--- src/main/resources/static/src/quotes/conversation.js
+++ src/main/resources/static/src/quotes/conversation.js
@@ -1,12 +1,24 @@
+import {computedFrom} from "aurelia-framework";
 import {json} from "aurelia-fetch-client";
+import {Logger} from "../util/cqrs-logging";
 
 class Conversation {
+
+  lines = [];
   constructor() {
-    this.lines = [];
   }
 
   addLine(line) {
     this.lines.push(line);
+  }
+
+  // @computedFrom("lines")
+  // get
+  hasLines() {
+    Logger.debug(`this.lines: ${this.lines}`);
+    Logger.debug(`[] == true: ${[] == true}`);
+    Logger.debug(`this.lines == true: ${this.lines == true}`);
+    return this.lines && this.lines.length > 0;
   }
 
   json() {
@@ -32,4 +44,4 @@ class Participant {
   }
 }
 
-export {Conversation, Line, Participant};
+export { Conversation, Line, Participant };
