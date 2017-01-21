983f842d15378c6a3d4390cc9977cd095ee8dc10
Tim Schraepen <sch3lp@gmail.com>
2017-01-21 22:24:42 +0100

    converting with readline

--- src/quotestext-2-json/converter.ts
+++ src/quotestext-2-json/converter.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as process from 'process';
+import * as readline from 'readline';
 
 !process.argv[2] && !process.argv[3]
     ? console.log(`You'll have to supply a filename as a second argument. e.g.:\nnode converter examplequotes.txt`)
@@ -8,13 +9,15 @@ import * as process from 'process';
 export function convert(sourceFile: string, targetFile: string) {
     if (!targetFile) {
         console.log(`No targetFile was given, using default`);
-        targetFile = 'output.txt';
+        targetFile = 'output.json';
     }
-    console.log(`Converting ${sourceFile} to ${targetFile}`);
-    fs.readFile(sourceFile, (err, data) => {
-        fs.writeFile(targetFile, data, (err) => err ? console.log(err) : true);
+    console.log(`Converting [${sourceFile}] to [${targetFile}]`);
+    let target = fs.createWriteStream(targetFile);
+
+    readline.createInterface({
+        input: fs.createReadStream(sourceFile)
+    })
+    .on('line', (line) => {
+        target.write(`${line}\n`);
     });
-    // fs.createReadStream(sourceFile).pipe(fs.createWriteStream('snarfsnarf.txt'));
-
 }
-
