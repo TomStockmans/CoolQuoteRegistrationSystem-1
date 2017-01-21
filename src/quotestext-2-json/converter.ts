ef2c4647c20cc228bc05bcdf51bca329b14f9c68
Tim Schraepen <sch3lp@gmail.com>
2017-01-21 23:12:01 +0100

    still figuring out TypeError: QuoteBuilder is not a constructor

--- src/quotestext-2-json/converter.ts
+++ src/quotestext-2-json/converter.ts
@@ -14,10 +14,66 @@ export function convert(sourceFile: string, targetFile: string) {
     console.log(`Converting [${sourceFile}] to [${targetFile}]`);
     let target = fs.createWriteStream(targetFile);
 
-    readline.createInterface({
-        input: fs.createReadStream(sourceFile)
-    })
-    .on('line', (line) => {
-        target.write(`${line}\n`);
+    let quoteBuilder = new QuoteBuilder();
+
+    const rl = readline.createInterface({input: fs.createReadStream(sourceFile)});
+    rl.on('line', (line) => {
+        if (!line) {
+            rl.emit('newline');
+        }
+        quoteBuilder.parseLine(line);
     });
+    rl.on('newline', () => {
+        quoteBuilder.writeQuote(target);
+    });
+}
+
+export class QuoteBuilder {
+    private quote: Quote;
+
+    constructor() {
+        this.quote = null;
+    }
+
+    parseLine(line: string) {
+        this.quote
+            ? this.quote.addLine(new Line(line))
+            : this.quote = new Quote(new Line(line));
+    }
+
+    writeQuote(target) {
+        target.write(JSON.stringify(this.quote));
+        this.quote = null;
+    }
+}
+
+class Line {
+    private text: string;
+    private author: string;
+
+    constructor(line: string) {
+        this.parse(line);
+    }
+
+    parse(line: string) {
+        let split = line.split(':');
+        if (split.length > 1) {
+            this.author = split[0];
+            this.text = split[1];
+        } else {
+            this.text = line;
+        }
+    }
+}
+
+class Quote {
+    private lines: Array<Line> = [];
+
+    constructor(line: Line) {
+        this.addLine(line);
+    }
+
+    addLine(line: Line): void {
+        this.lines.push(line);
+    }
 }
