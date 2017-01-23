514dd27b0be6fcf241f3d68bdec2f18e2164f942
Tim Schraepen <sch3lp@gmail.com>
2017-01-23 13:41:57 +0100

    order matters because classes don't get hoisted in ES2015

--- src/quotestext-2-json/converter.ts
+++ src/quotestext-2-json/converter.ts
@@ -2,6 +2,65 @@ import * as fs from 'fs';
 import * as process from 'process';
 import * as readline from 'readline';
 
+
+
+
+class QuoteBuilder {
+    private quote: Quote;
+
+    constructor() {
+        this.quote = null;
+    }
+
+    parseLine(line: string) {
+        this.quote
+            ? this.quote.addLine(new Line(line))
+            : this.quote = new Quote(new Line(line));
+    }
+
+    writeQuote(target) {
+        target.write(JSON.stringify(this.quote));
+        this.quote = null;
+    }
+}
+
+
+
+
+class Line {
+    private text: string;
+    private author: string;
+
+    constructor(line: string) {
+        this.parse(line);
+    }
+
+    parse(line: string) {
+        let split = line.split(':');
+        if (split.length > 1) {
+            this.author = split[0];
+            this.text = split[1];
+        } else {
+            this.text = line;
+        }
+    }
+}
+
+
+
+
+class Quote {
+    private lines: Array<Line> = [];
+
+    constructor(line: Line) {
+        this.addLine(line);
+    }
+
+    addLine(line: Line): void {
+        this.lines.push(line);
+    }
+}
+
 !process.argv[2] && !process.argv[3]
     ? console.log(`You'll have to supply a filename as a second argument. e.g.:\nnode converter examplequotes.txt`)
     : convert(process.argv[2], process.argv[3]);
@@ -26,54 +85,4 @@ export function convert(sourceFile: string, targetFile: string) {
     rl.on('newline', () => {
         quoteBuilder.writeQuote(target);
     });
-}
-
-export class QuoteBuilder {
-    private quote: Quote;
-
-    constructor() {
-        this.quote = null;
-    }
-
-    parseLine(line: string) {
-        this.quote
-            ? this.quote.addLine(new Line(line))
-            : this.quote = new Quote(new Line(line));
-    }
-
-    writeQuote(target) {
-        target.write(JSON.stringify(this.quote));
-        this.quote = null;
-    }
-}
-
-class Line {
-    private text: string;
-    private author: string;
-
-    constructor(line: string) {
-        this.parse(line);
-    }
-
-    parse(line: string) {
-        let split = line.split(':');
-        if (split.length > 1) {
-            this.author = split[0];
-            this.text = split[1];
-        } else {
-            this.text = line;
-        }
-    }
-}
-
-class Quote {
-    private lines: Array<Line> = [];
-
-    constructor(line: Line) {
-        this.addLine(line);
-    }
-
-    addLine(line: Line): void {
-        this.lines.push(line);
-    }
 }
