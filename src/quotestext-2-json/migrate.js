151cb792e456ece853b9ad33b842f894e08b3ae6
Tim Schraepen <sch3lp@gmail.com>
2017-01-27 16:39:33 +0100

    deleted unused

--- src/quotestext-2-json/migrate.js
+++
@@ -1,8 +0,0 @@
-db.conversation.insert([
-{"lines":[{"punchLine":false,"participants":[{"victim":false,"name":"Lion-O"}],"text":" SNARF! Get off that goddamn desktop!","lineType":"SPEECH"},{"punchLine":false,"text":"Snarf looks at Lion-O for a while","lineType":"CONTEXT"},{"punchLine":false,"participants":[{"victim":false,"name":"Snarf"}],"text":" snaaaarff snaaarffff!","lineType":"SPEECH"}],"_class":"be.swsb.cqrs.conversation.Conversation","createdOn":"2017-01-25T22:50:23.072Z"},{"lines":[{"punchLine":false,"participants":[{"victim":false,"name":"Snarf"}],"text":" Snarf snarf snarf?","lineType":"SPEECH"},{"punchLine":false,"participants":[{"victim":false,"name":"Lion-O"}],"text":" ohhh shut the fuck up snarf","lineType":"SPEECH"}],"_class":"be.swsb.cqrs.conversation.Conversation","createdOn":"2017-01-25T22:50:23.073Z"}
-]);
-        db.conversation.updateMany(
-            {},
-            { $set: {"createdOn": new Date()} }
-        );
-        
