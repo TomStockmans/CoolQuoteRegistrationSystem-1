5e461802fc320489d71aa9078b514e5b257e1437
TomStockmans <tom.stockmans@hotmail.com>
2022-07-21 15:44:17 +0200

    delete rubbish from mongodb

--- src/quotestext-2-json/migrateDates.js
+++
@@ -1,4 +0,0 @@
-db.conversation.find().forEach(function(c) {
-    c.createdOn = new Date();
-    db.conversation.save(c);
-});
--- src/quotestext-2-json/mongodb-extended-json/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * stringifies an object with native and/or BSON values back into
- * strict extended JSON.
- * @param  {Object} value               Object or value to stringify
- * @param  {Function|Array} replacer    Custom replacement
- * @param  {Number|String} space        Custom spacing
- * @return {String}                     JSON representation of value
- */
-export function stringify(object:Object, replacer?:Function|Array<any>, space?:number|string):string;
